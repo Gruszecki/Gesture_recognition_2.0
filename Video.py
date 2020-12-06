ec46e79d421e5fde9deddcb65cc0a097dd20ee6a
Gruszecki <wojtek.gruszecki@gmail.com>
2020-12-06 16:41:36 +0100

    Image conversion started

--- Video.py
+++ Video.py
@@ -9,12 +9,9 @@ class Video:
     def video_launch(self):
         while True:
             # Launching video
-            check, frame = self.video.read()                                    # Read image
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                     # Convert to grayscale
-            frame = cv2.blur(frame, (8, 8))                                     # Blur
-            frame = cv2.threshold(frame, 80, 255, cv2.THRESH_BINARY_INV)[1]     # B&W
-            # frame = cv2.Canny(frame, 100, 100)                                  # Contours
-            cv2.imshow("Gesture recognition 2.0", frame)                        # Show image
+            check, frame = self.video.read()                # Read image
+            frame = image_conversion(frame)                 # Image conversion
+            cv2.imshow("Gesture recognition 2.0", frame)    # Show image
 
             # Defining stopping requirement
             key = cv2.waitKey(1)
@@ -26,3 +23,10 @@ class Video:
         self.video.release()
         cv2.destroyAllWindows()
 
+
+def image_conversion(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                  # Convert to grayscale
+    # frame = cv2.blur(frame, (8, 8))                                  # Blur
+    # frame = cv2.threshold(frame, 80, 255, cv2.THRESH_BINARY_INV)[1]  # B&W
+    # frame = cv2.Canny(frame, 100, 100)                               # Contours
+    return frame
